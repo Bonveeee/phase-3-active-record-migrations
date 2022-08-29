4501eb48b86bda1beb95b07b57075d5ebacac0a3
bonveeee <bonochieng@gmail.com>
2022-08-29 14:38:40 +0300

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,10 @@
 # add the Artist class here
+class Artist < ActiveRecord::Base
+end
+
+# db/migrate/20210716100800_add_favorite_food_to_artists.rb
+class AddFavoriteFoodToArtists < ActiveRecord::Migration[6.1]
+    def change
+      add_column :artists, :favorite_food, :string
+    end
+  end
